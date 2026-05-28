07013a28f1d87e9b854f154a709f463224f3dd86
jekny <2656803448@qq.com>
2026-05-28 10:17:25 +0800

    编写MVC配置文件

---
+++ springMVC_all_code/springMVC_001_firstitem/src/main/java/com/hyw/mytest/controller/FirstController.java
@@ -0,0 +1,12 @@
+package com.hyw.mytest.controller;
+
+/**
+ * ClassName: FirstController
+ * Description:
+ *
+ * @Author jekny
+ * @Create 2026/5/28 10:15
+ * @Version 1.0
+ */
+public class FirstController {
+}
