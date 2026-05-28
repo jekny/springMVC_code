afc9c963dd2dfba7134e227bb160386c5b1df62f
jekny <2656803448@qq.com>
2026-05-28 15:13:22 +0800

    springMVC第一个程序打开成功

--- springMVC_all_code/springMVC_001_firstitem/src/main/java/com/hyw/mytest/controller/FirstController.java
+++ springMVC_all_code/springMVC_001_firstitem/src/main/java/com/hyw/mytest/controller/FirstController.java
@@ -1,5 +1,8 @@
 package com.hyw.mytest.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * ClassName: FirstController
  * Description:
@@ -8,5 +11,11 @@ package com.hyw.mytest.controller;
  * @Create 2026/5/28 10:15
  * @Version 1.0
  */
+@Controller
 public class FirstController {
+
+    @RequestMapping(value = "/haha")
+    public String haha(){
+        return "hehe";
+    }
 }
