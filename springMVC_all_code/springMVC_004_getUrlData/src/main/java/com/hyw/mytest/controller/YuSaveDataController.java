c47819afd3cbc6e4eae4c16fb984e72a4dc76a6e
jekny <2656803448@qq.com>
2026-06-04 12:30:58 +0800

    modelandview不用框架帮我们注入，是我们自己手动new出来的，返回值是modelandview而不是String，

--- springMVC_all_code/springMVC_004_getUrlData/src/main/java/com/hyw/mytest/controller/YuSaveDataController.java
+++ springMVC_all_code/springMVC_004_getUrlData/src/main/java/com/hyw/mytest/controller/YuSaveDataController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -29,8 +30,18 @@ public class YuSaveDataController {
     }
 
     @PostMapping("/test2")
-    public String test2(Model model){
+    public String test2(Model model){//这里的model和servlet，map，modelmap都得要spingMVC框架帮助我们进行注入，这里一共有5个方式，一个是servlet，map，model，modelmap，modelandview
+        //但是modelandview不用框架帮我们注入，是我们自己手动new出来的，返回值是modelandview而不是String，
         model.addAttribute("test22","我是数据");
         return "yu";
     }
+
+    @PostMapping("/test3")
+    public ModelAndView test3(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("test3","数据");
+        modelAndView.setViewName("yu");
+        return modelAndView;
+    }
+
 }
