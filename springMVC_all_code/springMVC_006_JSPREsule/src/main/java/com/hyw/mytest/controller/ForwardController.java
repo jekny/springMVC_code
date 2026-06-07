3f89ca0f23d49186480b15d052c8f8b1f569870d
jekny <2656803448@qq.com>
2026-06-07 11:18:45 +0800

    forward资源转发

---
+++ springMVC_all_code/springMVC_006_JSPREsule/src/main/java/com/hyw/mytest/controller/ForwardController.java
@@ -0,0 +1,29 @@
+package com.hyw.mytest.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+/**
+ * ClassName: ForwardController
+ * Description:
+ *
+ * @Author jekny
+ * @Create 2026/6/7 11:08
+ * @Version 1.0
+ */
+@Controller
+public class ForwardController {
+    @PostMapping({"/a"})
+    public String a(HttpServletRequest request){
+        System.out.println("a方法执行");
+        request.setAttribute("name","张三");
+        return "forward:/b";
+    }
+
+    @PostMapping("/b")
+    public String b(){
+        System.out.println("b方法执行");
+        return "test";
+    }
+}
