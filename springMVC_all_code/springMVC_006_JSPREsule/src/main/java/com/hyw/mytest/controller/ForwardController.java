56dac3fb3f84e37b377a795cc5b5eb3e86c9fc4f
jekny <2656803448@qq.com>
2026-06-07 11:24:16 +0800

    redirect重定向，两次请求，跨域访问其他服务器中的资源时可用，在redirect:后面写上完整的路径http://...，资源转发是访问内部资源

--- springMVC_all_code/springMVC_006_JSPREsule/src/main/java/com/hyw/mytest/controller/ForwardController.java
+++ springMVC_all_code/springMVC_006_JSPREsule/src/main/java/com/hyw/mytest/controller/ForwardController.java
@@ -2,6 +2,8 @@ package com.hyw.mytest.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 /**
@@ -18,10 +20,10 @@ public class ForwardController {
     public String a(HttpServletRequest request){
         System.out.println("a方法执行");
         request.setAttribute("name","张三");
-        return "forward:/b";
+        return "redirect:/b";
     }
 
-    @PostMapping("/b")
+    @GetMapping("/b")
     public String b(){
         System.out.println("b方法执行");
         return "test";
